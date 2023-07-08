eaf4180025d837f15b8024307e264f31ee6c6847
Senol <senoltr@yandex.com>
2023-07-08 10:11:19 +0300

    First Commit

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,26 @@
+import { ProductResponseModel } from './../../models/productResponseModel';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  dataLoaded: boolean = false;
+  constructor(private productService:ProductService) {} //Not: DI bu yapı hazır olarak angular içinde gelmektedir.
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe(response => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
+  }
+}

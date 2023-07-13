8de04c71a43accbe0afb0e579474a0f99b407447
Senol <senoltr@yandex.com>
2023-07-13 10:18:19 +0300

    All Category Bug Fixed !

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,7 +1,6 @@
+import { Component, OnInit } from '@angular/core';
 import { Category } from './../../models/category';
 import { CategoryService } from './../../services/category.service';
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-category',
@@ -9,38 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-  categories:Category[] = [];
-  currentCategory:Category;
-  constructor(private categoryService:CategoryService) {}
-  
-  
+  categories: Category[] = [];
+  currentCategory: Category | null;
+  allCategoryClicked: boolean = false;
+
+  constructor(private categoryService: CategoryService) {}
+
   ngOnInit(): void {
+    //CurrentCategory'i null'a çekiyorum ve aşağıdaki class'ı çalıştırıyorum.
+    this.setCurrentCategory(null);
     this.getCategories();
   }
 
-  getCategories(){
+  getCategories() {
     return this.categoryService.getCategories().subscribe(response => {
       this.categories = response.data;
-    })
+    });
   }
 
-  setCurrentCategory(category:Category){
+  setCurrentCategory(category: Category | null) {
     this.currentCategory = category;
+    this.allCategoryClicked = false;
   }
 
-  getCurrentCategoryClass(category:Category){
-    if(category == this.currentCategory){
-      return "list-group-item active"
+  getCurrentCategoryClass(category: Category) {
+    if (category === this.currentCategory) {
+      return "list-group-item active";
     }
-    return "list-group-item"
+    return "list-group-item";
   }
 
-  getAllCategoryClass(){
-    if(!this.currentCategory){
-      return "list-group-item active"
-    }else{
-      return "list-group-item"
+  getAllCategoryClass() {
+    if (this.currentCategory === null && !this.allCategoryClicked) {
+      return "list-group-item active";
+    } else {
+      return "list-group-item";
     }
-
   }
-  }
+}
